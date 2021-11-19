ca0134243972e2cae6cde65623b07dacb0a7684d
vivekvincent <vivekvncnt@gmail.com>
2021-11-19 21:09:57 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,30 @@
+print("\n\n\nwelcome to vivek's calculator")
+
+
+a=input ("\n\nEnter the first numnber: ")
+b=input ("\n\nEnter the second numnber: ")
+
+c=float(a) + float(b)
+d=float(a)*float(b)
+e=float(a)-float(b)
+f=float(a)/float(b)
+
+
+print("\n\nnThe sum of numbers is", c)
+
+print("\nThe Difference of numbers is", e)
+
+print("\nThe multiplication of numbers is", d)
+
+print("\nThe division value of numbers is", f)
+
+print("\n")
+
+
+def myfunc(name1):
+
+	print("hello", name1)
+
+myfunc("\"vivek vincent\"")
+
+
